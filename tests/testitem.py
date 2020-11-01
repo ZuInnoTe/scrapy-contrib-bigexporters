afe02baab14d083b7f4fb62e14660ae3ab738248
Jörn Franke <jornfranke@gmail.com>
2020-11-01 22:08:03 +0100

    added avrotest

---
+++ tests/testitem.py
@@ -0,0 +1,33 @@
+"""
+Copyright (c) 2020 ZuInnoTe (Jörn Franke) <zuinnote@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import scrapy
+from itemloaders.processors import Join, MapCompose, TakeFirst
+
+class TestItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    ftext = scrapy.Field(output_processor=TakeFirst())
+    ftext_array = scrapy.Field()
+    ffloat = scrapy.Field(output_processor=TakeFirst())
+    fint = scrapy.Field(output_processor=TakeFirst())
+    fbool = scrapy.Field(output_processor=TakeFirst())
+    fdatetime = scrapy.Field(output_processor=TakeFirst())
