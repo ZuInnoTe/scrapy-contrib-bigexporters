a01d88515cb4b94db091f227ca834791943a0977
Jörn Franke <jornfranke@gmail.com>
2020-11-03 21:32:29 +0100

    Added convertstr test

--- tests/testparquet.py
+++ tests/testparquet.py
@@ -54,6 +54,14 @@ class TestParquetItemExporter(unittest.TestCase):
         itemExporter=ParquetItemExporter(file=self.file,compression= 'GZIP',times= 'int64', hasnulls=True,convertallstrings= False,writeindex= False,rowgroupoffset=50000000,items_rowgroup=10000)
         self.export_type_schema(itemExporter)
 
+    def test_parquet_export_string_schema(self):
+        """
+            Test if file is correctly written with strings
+        """
+        # create exporter
+        itemExporter=ParquetItemExporter(file=self.file,compression= 'GZIP',times= 'int64', hasnulls=True,convertallstrings= True,writeindex= False,rowgroupoffset=50000000,items_rowgroup=10000)
+        self.export_string_schema(itemExporter)
+
     def test_parquet_export_type_schema_recordcache(self):
         """
             Tests if all records are written even if number is larger than record cache
@@ -94,6 +102,38 @@ class TestParquetItemExporter(unittest.TestCase):
                        self.assertEqual(datetime_str,datetime.datetime.fromtimestamp(record.get('fdatetime',None)).strftime(datetime_fmt),msg="DateTime data is read correctly")
                        rcount+=1
                self.assertEqual(rcount,10,msg="Number of records read corresponds to number of records written")
+
+    def export_string_schema(self,itemExporter):
+           itemExporter.start_exporting()
+           # create and write some test data
+           num_records=10
+           for i in range(num_records):
+                l = ItemLoader(TestItem())
+                l.add_value('ftext','this is a test text')
+                l.add_value('ftext_array',['test1','test2','test3','test4'])
+                l.add_value('ffloat',float(2.5))
+                l.add_value('fint',int(10))
+                l.add_value('fbool',False)
+                datetime_str = '2020-02-29T11:12:13'
+                datetime_fmt='%Y-%m-%dT%H:%M:%S'
+                datetime_obj = datetime.datetime.strptime(datetime_str,datetime_fmt)
+                l.add_value('fdatetime',datetime_obj.timestamp())
+                itemExporter.export_item(l.load_item())
+           itemExporter.finish_exporting()
+           self.file.close()
+           # reread file and compare results
+           rcount=0
+           pf = ParquetFile(self.filename)
+           df=pf.to_pandas()
+           for indx,record in df.iterrows():
+                   self.assertEqual("this is a test text",record.get('ftext',None),msg="String data is read correctly")
+                   self.assertEqual("['test1', 'test2', 'test3', 'test4']",record.get('ftext_array',None),msg="String array data is read correctly")
+                   self.assertEqual("2.5",record.get('ffloat',None),msg="Float data is read correctly")
+                   self.assertEqual("10",record.get('fint',None),msg="Int data is read correctly")
+                   self.assertEqual("False",record.get('fbool',None),msg="Bool data is read correctly")
+                   self.assertEqual("1582971133.0",record.get('fdatetime',None),msg="DateTime data is read correctly")
+                   rcount+=1
+           self.assertEqual(rcount,10,msg="Number of records read corresponds to number of records written")
 
 if __name__ == '__main__':
     unittest.main()
